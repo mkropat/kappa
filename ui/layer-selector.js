0943cb783fa40ba68034b93b7715781d4582c5f5
Michael Kropat <mail@michael.kropat.name>
2016-08-26 02:02:57 +0000

    Some layer editor ui stuff

--- ui/layer-selector.js
+++ ui/layer-selector.js
@@ -6,6 +6,7 @@
       super(props);
 
       this.state = {
+        activeIndex: 0,
         layers: [],
       };
 
@@ -21,20 +22,29 @@
     }
 
     addLayer() {
-      let layers = this.state.layers;
+      let layers = this.state.layers.slice();
       layers.push({ foo: 123 });
 
       this.props.socket.emit('set-layers', layers);
     }
 
+    removeLayer(i) {
+      let layers = this.state.layers.slice();
+      layers.splice(i, 1);
+      this.props.socket.emit('set-layers', layers);
+    }
+
     render() {
       var items = this.state.layers.map((l, i) =>
-          React.createElement('li', null, 'layer ' + i));
+          React.createElement('a', { className: 'list-group-item' + (this.state.activeIndex === i ? ' active' : ''), onClick: () => this.setState({ activeIndex: i }) },
+            'layer ' + i,
+            React.createElement('button', { className: 'btn btn-link', onClick: () => this.removeLayer(i) },
+              React.createElement('i', { className: 'glyphicon glyphicon-trash' }))));
 
       return React.createElement('div', null,
-          React.createElement('h3', null, 'Layer Selector'),
-          React.createElement('ul', null, items),
-          React.createElement('button', { onClick: this.addLayer }, 'Add Layer'));
+          React.createElement('h4', null, 'Layers'),
+          React.createElement('div', { className: 'list-group' }, items),
+          React.createElement('button', { onClick: this.addLayer, className: 'btn' }, 'Add Layer'));
     }
   };
 
